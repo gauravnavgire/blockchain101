fe9609528eaa5a4dc176fa74ec9b95fc34ad86ae
gaurav <gaurav@rubix108.com>
2018-10-18 03:25:14 +0530

    Initial Commit

---
+++ contracts/Election.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+contract Election{
+	
+	// Store Condidate
+	// Read Candidate
+	string public candidate;
+	// Constructor
+	constructor() public {
+		candidate = "Candidate 1";
+	}
+	
+
+	
+}
